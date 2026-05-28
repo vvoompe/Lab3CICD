152abbfe58f064d784d1594fa48940748380b94c
vvoompe <alex.markgraf76@gmail.com>
2026-05-28 22:21:29 +0300

    fix: add STATIC_ROOT and allow all hosts for Heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pu20*571c71g1v2qncvx4jn(md*$n+#u-xv22dzf-)-1k9@ylo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
